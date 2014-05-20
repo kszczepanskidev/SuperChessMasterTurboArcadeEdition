0d52a86de8e40660bc446d3c2ee90170c19b39b8
Krysztal <krysztalzg@gmail.com>
2014-05-20 02:47:31 +0200

    Fully reading
    Completed reading from png file.
    TO DO:
    -change ifs to lambda switch in readBasicInfo()

--- pgn/pgn/pgn.cpp
+++ pgn/pgn/pgn.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 #include "pgn.h"
 
@@ -10,7 +11,6 @@ int main(int argc, char* argv[]) {
 	string filename,temp;
 	ifstream pgnfile;
 	Match *newGame = new Match();
-
 	cout << "Enter .PGN file name: ";
 	cin >> filename;
 
@@ -21,8 +21,11 @@ int main(int argc, char* argv[]) {
 	}
 	else {
 		while (getline(pgnfile, temp) && !temp.empty()) {
-			read(temp, newGame);
+			readBasicInfo(temp, newGame);
 		}
+		while (getline(pgnfile, temp))
+			readMoves(temp,newGame);
+
 		pgnfile.close();
 	}
 	write(newGame);
@@ -31,24 +34,48 @@ int main(int argc, char* argv[]) {
 }
 
 
-void read(string in, Match* game) {
+void readBasicInfo(string in, Match* game) {
 	string type,value;
 
 	type = in.substr(1, in.find(char(32)) - 1);
 	value = in.substr(in.find(char(34)) + 1, (in.length() - in.find(char(34)) - 3));
 
-	if (type == "Event") game->name = value;
-	if (type == "Site") game->place = value;
-	if (type == "Date") game->date = value;
-	if (type == "Round") game->round = value;
-	if (type == "White") game->whitePlayer = value;
-	if (type == "Black") game->blackPlayer = value;
-	if (type == "Result") game->result = value;
-	if (type == "ECO") game->eco = value;
+	if (type == "Event")	game->name = value;
+	if (type == "Site")		game->place = value;
+	if (type == "Date")		game->date = value;
+	if (type == "Round")	game->round = value;
+	if (type == "White")	game->whitePlayer = value;
+	if (type == "Black")	game->blackPlayer = value;
+	if (type == "Result")	game->result = value;
+	if (type == "ECO")		game->eco = value;
 	if (type == "WhiteElo") game->whiteElo = value;
 	if (type == "BlackElo") game->blackElo = value;
-	if (type == "Annotator") game->annotator = value;
+	if (type == "Annotator")game->annotator = value;
 }
+
+void readMoves(string temp, Match* game){
+	size_t resultFound = 0, found = 0, separator = 0;
+	while (!temp.empty()) {
+		found = temp.find(".", 3) - 2;
+		if ((int)found < 0) {
+			found = temp.length();
+			if (((resultFound = temp.find("1/2-1/2")) != -1) ||
+				((resultFound = temp.find("1-0")) != -1) ||
+				((resultFound = temp.find("0-1")) != -1))
+				found = resultFound;
+		}
+		if (temp[found] != ' ' && found != temp.length()) 
+			found--;
+		separator = temp.find(" ") + 1;
+		//cout << temp.substr(separator, found-separator) << endl;
+		game->moves.push_back(temp.substr(separator, found - separator));
+			if(temp[found] == ' ')	found++;
+				temp = temp.substr(found);
+				if (found >= temp.length())
+			temp = "";
+	}
+}
+
 
 void write(Match* game) {
 	cout << endl << endl;
@@ -74,4 +101,10 @@ void write(Match* game) {
 		cout << "Black pawns's player's Elo: " << game->blackElo << endl;
 	if (!game->annotator.empty()) 
 		cout << "Annotator's name: " << game->annotator << endl;
+
+	if (!game->moves.empty()) {
+		cout << endl << "Moves:" << endl;
+		for (string move : game->moves)
+			cout << move << endl;
+	}
 }
--- pgn/pgn/pgn.h
+++ pgn/pgn/pgn.h
@@ -16,11 +16,13 @@ public:
 	string whiteElo;
 	string blackElo;
 	string annotator;
-
+	vector<string> moves;
 	//array of moves or sth
 };
 
-void read(string temp, Match* game);
+void readBasicInfo(string temp, Match* game);
+void readMoves(string temp, Match* game);
+
 void write(Match* game);
 
 #endif
