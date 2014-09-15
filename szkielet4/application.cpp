e3c27cb3b59f2638af772c1e34189359280cc8df
krysztalzg <krysztalzg@gmail.com>
2014-09-15 16:28:03 +0200

    Its enumarated!

--- szkielet4/application.cpp
+++ szkielet4/application.cpp
@@ -40,34 +40,42 @@ Application::Application(ShaderProgram *shader) {
 			chessBoard->squares[i][j].x = (3.5 - j)*0.5f;
 			chessBoard->squares[i][j].y = (-3.5 + i)*0.5f;
 		}
+	for (int i = 0; i < 8; ++i)
+		for (int j = 0; j < 8; j++) {
+			if (j<2 || j>5)
+				chessBoard->squares[i][j].empty = false;
+			else
+				chessBoard->squares[i][j].empty = true;
+		}
 
 	//Init pieces
 	//Pawns
 	for (int i = 0; i < 8; ++i) {
 		pieces[i] = new Piece(models[0], &textures[0], &textures[3], chessBoard->squares[i][1], 'p');
-		pieces[i+8] = new Piece(models[0], &textures[1], &textures[3], chessBoard->squares[i][6], 'p');
+		pieces[i+16] = new Piece(models[0], &textures[1], &textures[3], chessBoard->squares[i][6], 'p');
 	}
 	//Knights
-	pieces[16] = new Piece(models[1], &textures[0], &textures[3], chessBoard->squares[1][0], 'N');
-	pieces[17] = new Piece(models[1], &textures[0], &textures[3], chessBoard->squares[6][0], 'N');
-	pieces[18] = new Piece(models[1], &textures[1], &textures[3], chessBoard->squares[1][7], 'N');
-	pieces[19] = new Piece(models[1], &textures[1], &textures[3], chessBoard->squares[6][7], 'N');
+	pieces[8] = new Piece(models[1], &textures[0], &textures[3], chessBoard->squares[1][0], 'N');
+	pieces[9] = new Piece(models[1], &textures[0], &textures[3], chessBoard->squares[6][0], 'N');
+	pieces[24] = new Piece(models[1], &textures[1], &textures[3], chessBoard->squares[1][7], 'N');
+	pieces[25] = new Piece(models[1], &textures[1], &textures[3], chessBoard->squares[6][7], 'N');
 	//Queens
-	pieces[20] = new Piece(models[2], &textures[0], &textures[3], chessBoard->squares[3][0], 'Q');
-	pieces[21] = new Piece(models[2], &textures[1], &textures[3], chessBoard->squares[3][7], 'Q');
+	pieces[10] = new Piece(models[2], &textures[0], &textures[3], chessBoard->squares[3][0], 'Q');
+	pieces[26] = new Piece(models[2], &textures[1], &textures[3], chessBoard->squares[3][7], 'Q');
 	//Bishops
-	pieces[22] = new Piece(models[3], &textures[0], &textures[3], chessBoard->squares[2][0], 'B');
-	pieces[23] = new Piece(models[3], &textures[0], &textures[3], chessBoard->squares[5][0], 'B');
-	pieces[24] = new Piece(models[3], &textures[1], &textures[3], chessBoard->squares[2][7], 'B');
-	pieces[25] = new Piece(models[3], &textures[1], &textures[3], chessBoard->squares[5][7], 'B');
+	pieces[11] = new Piece(models[3], &textures[0], &textures[3], chessBoard->squares[2][0], 'B');
+	pieces[12] = new Piece(models[3], &textures[0], &textures[3], chessBoard->squares[5][0], 'B');
+	pieces[27] = new Piece(models[3], &textures[1], &textures[3], chessBoard->squares[2][7], 'B');
+	pieces[28] = new Piece(models[3], &textures[1], &textures[3], chessBoard->squares[5][7], 'B');
 	//Rooks
-	pieces[26] = new Piece(models[4], &textures[0], &textures[3], chessBoard->squares[0][0], 'R');
-	pieces[27] = new Piece(models[4], &textures[0], &textures[3], chessBoard->squares[7][0], 'R');
-	pieces[28] = new Piece(models[4], &textures[1], &textures[3], chessBoard->squares[0][7], 'R');
-	pieces[29] = new Piece(models[4], &textures[1], &textures[3], chessBoard->squares[7][7], 'R');
+	pieces[13] = new Piece(models[4], &textures[0], &textures[3], chessBoard->squares[0][0], 'R');
+	pieces[14] = new Piece(models[4], &textures[0], &textures[3], chessBoard->squares[7][0], 'R');
+	pieces[29] = new Piece(models[4], &textures[1], &textures[3], chessBoard->squares[0][7], 'R');
+	pieces[30] = new Piece(models[4], &textures[1], &textures[3], chessBoard->squares[7][7], 'R');
 	//Kings
-	pieces[30] = new Piece(models[5], &textures[0], &textures[3], chessBoard->squares[4][0], 'K');
+	pieces[15] = new Piece(models[5], &textures[0], &textures[3], chessBoard->squares[4][0], 'K');
 	pieces[31] = new Piece(models[5], &textures[1], &textures[3], chessBoard->squares[4][7], 'K');
+
 
 	//Init moves
 	match = new Match();
